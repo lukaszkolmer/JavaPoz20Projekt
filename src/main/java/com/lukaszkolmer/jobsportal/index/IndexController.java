44d65bd2790e4cac1be2f7dfe1fbcc35b72663f0
Lukasz Kolmer <sephii123@gmail.com>
2020-05-29 16:41:28 +0200

    index.html now properly show number of job offers in base, added get mapping in index class and thymeleaf function in index.html 102

--- src/main/java/com/lukaszkolmer/jobsportal/index/IndexController.java
+++ src/main/java/com/lukaszkolmer/jobsportal/index/IndexController.java
@@ -1,13 +1,24 @@
 package com.lukaszkolmer.jobsportal.index;
-
+import com.lukaszkolmer.jobsportal.jobs.model.JobDetails;
+import com.lukaszkolmer.jobsportal.jobs.repository.JobDetailsRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    JobDetailsRepositoryImpl jobDetailsRepository;
+
     @GetMapping({"/","/index.html"})
-    public String getIndex(){
+    public String getIndex(Model model){
+        List<JobDetails> listOfJobOffers = jobDetailsRepository.getJobDetailsRepository().findAll();
+        model.addAttribute("listOfJobOffers",listOfJobOffers);
+
         return "index";
     }
 }
