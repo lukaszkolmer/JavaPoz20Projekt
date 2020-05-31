21aaae2632fcad1781273992cd5823fa8f403114
Lukasz Kolmer <sephii123@gmail.com>
2020-05-31 17:36:26 +0200

    clicking on offer in index.html and jobs.html now correctly send to offer of given id. Added th hrefs in index.html 264 and jobs.html 229

--- src/main/java/com/lukaszkolmer/jobsportal/jobs/controller/JobsController.java
+++ src/main/java/com/lukaszkolmer/jobsportal/jobs/controller/JobsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,9 +25,9 @@ public class JobsController {
     }
 
     @GetMapping("JobDetails.html") // inaczej nie działa, duże znaki w adresie?
-    public String getJobsDetails(Model model, Long id){
+    public String getJobsDetails(Model model, @RequestParam Long id){
 
-        JobDetails jobDetails = jobDetailsRepository.findOfferById(1L); // chwilowo działa jeszcze na sztywno podawanym id.
+        JobDetails jobDetails = jobDetailsRepository.findOfferById(id); // chwilowo działa jeszcze na sztywno podawanym id.
         model.addAttribute("jobOffer",jobDetails);
 
         return "job_details";
