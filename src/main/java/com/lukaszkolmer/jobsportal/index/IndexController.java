f366653c67cb6ead118bc9d6975da65a50de9ee7
Lukasz Kolmer <sephii123@gmail.com>
2020-06-09 19:34:54 +0200

    added thymeleaf config class for security dialect. Log in option in css now correctly logs in user and changes to profile and logout

--- src/main/java/com/lukaszkolmer/jobsportal/index/IndexController.java
+++ src/main/java/com/lukaszkolmer/jobsportal/index/IndexController.java
@@ -1,7 +1,12 @@
 package com.lukaszkolmer.jobsportal.index;
 import com.lukaszkolmer.jobsportal.jobs.model.JobDetails;
 import com.lukaszkolmer.jobsportal.jobs.repository.JobDetailsRepositoryImpl;
+import com.lukaszkolmer.jobsportal.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +20,15 @@ public class IndexController {
 
     @Autowired
     JobDetailsRepositoryImpl jobDetailsRepository;
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @GetMapping({"/","/index.html"})
     public String getIndex(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         List<JobDetails> listOfJobOffers = jobDetailsRepository.getJobDetailsRepository().findAll();
         model.addAttribute("listOfJobOffers",listOfJobOffers);
-
+        model.addAttribute("user", new User());
         return "index";
     }
 }
